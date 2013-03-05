8dd8e29ab0374e913384a6be415e28a942cd4aa4
duncan <duncan@localhost.localdomain>
2013-03-05 11:37:51 -0700

    fixed the search links stream

--- src/searchengine/SearchLinks.java
+++ src/searchengine/SearchLinks.java
@@ -6,6 +6,7 @@ package searchengine;
 
 import com.its.util.Parser;
 import com.its.util.Stringer;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,21 +14,19 @@ import java.util.List;
  * @author duncan
  */
 public class SearchLinks extends SearchEngine {
-        
-    public SearchLinks(String url){
-        super(url); 
+
+    public SearchLinks(String url) {
+        super(url);
     }
 
     public SearchLinks(String url, String keywords) {
         super(url, keywords);
     }
-        
 
-    public List<String[]> findLinks()
-    {
+    public List<String[]> findLinks() {
         //define strings    
         String[] links;
-        List<String[]> results = null;
+        List<String[]> results = new ArrayList<>();
         readWebPage();
 
         //build an array out of the links in tha page 
@@ -35,77 +34,79 @@ public class SearchLinks extends SearchEngine {
 
         // for every link in the array
         for (String link : links) {
-                link = Parser.parseWithPatterns(link,"before,</a>");
-                /*
-                 * get the URL
-                 */    
+            link = Parser.parseWithPatterns(link, "before,</a>");
+            /*
+             * get the URL
+             */
 
 
-                //get the index of the first carictor of the link destanation 
-                int destIndex = link.indexOf("href=")+5;//TODO what if href= dose not exits?
-                int destIndexEnd;// the last caricto of the destanation
+            //get the index of the first carictor of the link destanation 
+            int destIndex;//TODO what if href= dose not exits?
+            if(link.indexOf("href=") >= 0) {
+                destIndex = link.indexOf("href=") + 5;
+            } else {
+                continue;
+            }
+            int destIndexEnd;// the last caricto of the destanation
 
-                String desntanation;
+            String desntanation;
 
-                //find the end of the string       
-                //find the witch is closer a space or a end dimond 
-                destIndexEnd = (link.indexOf(" ", destIndex)<link.indexOf(">", destIndex))?
-                         link.indexOf(" ", destIndex): link.indexOf(">", destIndex);
-
-                //if it has a qotation mark
-                if(link.charAt(destIndex+1)=='"'||link.charAt(destIndex+1)=='\''){
-                    destIndex+=1;
-                    destIndexEnd-=1;
-                }
+            //find the end of the string       
+            //find the witch is closer a space or a end dimond 
+            destIndexEnd = link.indexOf(">", destIndex);
+            //if it has a qotation mark
+            if (link.charAt(destIndex) == '"' || link.charAt(destIndex) == '\'') {
+                desntanation = link.substring(destIndex + 1, destIndexEnd - 1);
+            } else {
                 desntanation = link.substring(destIndex, destIndexEnd);
+            }
+            /*
+             * get the lable 
+             */
 
-                /*
-                 * get the lable 
-                 */
+            //get the index of the first carictor of the link destanation 
+            int labelIndex = link.indexOf(">") + 1;//TODO what if href= dose not exits?
+            int labelIndexEnd; // the last caricto of the destanation
 
-                //get the index of the first carictor of the link destanation 
-                int labelIndex = link.indexOf(">");//TODO what if href= dose not exits?
-                int labelIndexEnd;// the last caricto of the destanation
+            String label;
 
-                String label;
-
-                //find the end of the string 
+            //find the end of the string 
+            
+            if(link.indexOf("<",labelIndex)<=0){
+                labelIndexEnd = link.length();
+            }
+            else{
                 labelIndexEnd = link.indexOf("<", labelIndex);
-                label = link.substring(labelIndex, labelIndexEnd);
+            }
+            label = link.substring(labelIndex, labelIndexEnd);
 
-                /*
-                 * get the score
-                 */
+            /*
+             * get the score
+             */
 
-                //System.out.println(sentences[i]);
-                int counter = 0;
-                for (String word: words)
-                {
-                        if(link.indexOf(word) >= 0) 
-                        {
-                                counter++;
-                        }
+            //System.out.println(sentences[i]);
+            int counter = 0;
+            for (String word : words) {
+                if (label.indexOf(word) >= 0) {
+                    counter++;
                 }
-                //cancatanate the values 
-                if(counter==0){
-                    break;
-                }
-                results.add(new String[]{desntanation, label, counter+""});
+            }
+            if (counter == 0) {
+                continue;
+            }
+            //cancatanate the values 
+            results.add(new String[]{desntanation,label,counter+""});
         }
         return results;
     }
 
-    public void resultsWriter()
-    {
-            System.out.println(findLinks());
+    public void resultsWriter() {
+        System.out.println(findLinks());
     }
 
-    public static void main (String[] Args)
-    {
-            SearchLinks test = new SearchLinks("http://netbeans.org/kb/docs/java/quickstart.html");
-            test.resultsWriter();
+    public static void main(String[] Args) {
+        SearchLinks test = new SearchLinks("http://netbeans.org/kb/docs/java/quickstart.html");
+        test.setKeyWordsWithInput();
+        test.resultsWriter();
     }
-
-    
-
 }
