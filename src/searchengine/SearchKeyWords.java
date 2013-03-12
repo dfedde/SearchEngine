9c8ed7237e89363e514a5bd4c05fc735b5bf582e
duncan <duncan@localhost.localdomain>
2013-03-12 07:17:17 -0600

    added the search pages methoud

--- src/searchengine/SearchKeyWords.java
+++ src/searchengine/SearchKeyWords.java
@@ -3,13 +3,12 @@ package searchengine;
 import com.its.util.Stringer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class SearchKeyWords extends SearchEngine
 {
 
 	private String[]    lineEnders  = {".","?","!"};
-	private Scanner     keyboard    = new Scanner(System.in);
+
 
         public SearchKeyWords(String url) {
             super(url);
@@ -31,73 +30,59 @@ public class SearchKeyWords extends SearchEngine
 		
 	}
 	
-	public void setKeyWordsWithInput()
-	{
-                String keywords;
-		System.out.println("Please enter your search criteria...");
-		keywords = keyboard.nextLine();
-		words = Stringer.split(" ", keywords);
-	}
-	
-	
-	public String searchTextSentences()
+	public List<String[]> searchTextSentences()
 	{
 		readWebPage();
                 String[] sentences = Stringer.split(getPage(),  lineEnders, true);
-		String results = "";
-		List<String> relevanceList = new ArrayList<String>();
-		int numberOfSentences = sentences.length;
-		for (int i = 0; i < numberOfSentences; i++)
-		{	
-			//System.out.println(sentences[i]);
-			String sentence = sentences[i];
-			int counter = 0;
-			for (int j = 0; j < words.length; j++)
-			{
-				if(sentence.indexOf(words[j]) >= 0) 
-				{
-					counter++;
-				}
-			}
-			relevanceList.add(counter+"");
-		}
-		int sortColumnIndex = 1;
-		List<String[]> listOfStringArrays = new ArrayList<String[]>();
-		//concatanate sting with relavance counter 
-                for(int i = 0; i< numberOfSentences; i++)
-		{
-			String sentence = sentences[i];
-			String relevanceCounter = relevanceList.get(i);
-			String[] sentenceAndCounter = {sentence, relevanceCounter};
-			listOfStringArrays.add(sentenceAndCounter);
-		}
-		List<String[]> listOfSortedArrays = Stringer.sort(listOfStringArrays, sortColumnIndex);
-		for(int i = 0; i < listOfSortedArrays.size(); i++)
-		{
-			String[] sentenceAndScore = listOfSortedArrays.get(i);
-			String sentence = sentenceAndScore[0];
-			String score = sentenceAndScore[1];
-			if(!score.equals("0"))
-			{
-			results += sentence + " : " + score;
-			}
-		}
-		return results;
+		List<String[]> listOfStringArrays = new ArrayList<>();
+                
+                for (String sentence : sentences){ 	
+                    //System.out.println(sentence);
+                    int counter = 0;
+                    for (String word: words)
+                    {
+                            if(sentence.indexOf(word) >= 0) 
+                            {
+                                    counter++;
+                            }
+                    }
+                    if(counter == 0 ){
+                        continue;
+                    }
+                    System.out.println(sentence+": "+counter);
+                    listOfStringArrays.add(new String[]{sentence, counter+""});
+                }
+		return listOfStringArrays;
 	}
-	
-
-	// write the web sorse to a file to tomcat 
-	public void resultsWriter()
-	{
-		
-	}
-	
-	
+        /**
+         * returns a list of links with a snipit off the page
+         * @param list of links
+         * @return link and snipit of page {url,snipit}
+         * @calls searchTextSenteces
+         */
+        public List<String[]> searchPages(List<String[]> listOfLinks){
+            List<String[]> resaults = new ArrayList<>();
+            //call searchtextsentics for evry link and make aray for each
+            for(String[] link: listOfLinks){
+                //array to store the data from search textsentices
+                List<String[]> relaventResaults = new ArrayList<>();
+                //get the url
+                String url = link[1];
+                //set the new url 
+                setUrl(url);
+                //get the resaults
+                relaventResaults = searchTextSentences();
+                // get the fist to resalts and concatanate them
+                resaults.add(new String[]{url, relaventResaults.get(1)[1]+
+                        "\n"+relaventResaults.get(2)[1] });
+            }
+            return resaults;
+        }
 	public static void main(String[] args)
 	{
-            SearchKeyWords search = new SearchKeyWords("www.google.com");
+            SearchKeyWords search = new SearchKeyWords("http://www.pageinc.org/");
             search.setKeyWordsWithInput();
-            System.out.print(search.searchTextSentences());
+            System.out.print(StringResults(search.searchTextSentences()));
 	}
 	
 }
