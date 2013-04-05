ece8cf9b8e061790a3074b5195dbf4351c62053f
Julian <jln.whitley@gmail.com>
2013-04-04 20:08:51 -0600

    scrapeWords updates

--- searchengine/SearchKeyWords.java
+++ searchengine/SearchKeyWords.java
@@ -1,8 +1,15 @@
 package searchengine;
 
 import com.its.util.Stringer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class SearchKeyWords extends SearchEngine {
@@ -90,14 +97,97 @@ public class SearchKeyWords extends SearchEngine {
         }
         return resaults;
     }
-/**
- * this function takes a list of urls and 
- * list of unique keywords from each. the key word must also not be in the
- * stop-words table
- * @param String[] a list of unique urls;
- * @return String[][]  an array of ["url", "keyword1", "keyword2", ...]
- *	for each url. array is null if page has an error
- */
+
+
+    /**   version 2
+	 * This function will take a list of urls and 
+	 * pull ALL of the words from each page into an array list, 
+	 * which will then be converted into an array. 
+	 * The returned array will be a two dimensional array 
+	 * which emulates the DB
+	 * @param String[] a list of unique urls;
+	 * @return String[String[]  an array of ["url", "keyword1", "keyword2", ...]
+	 * for each url. array is null if page has an error
+	 */	 
+		public String[] scrapeWords3(String[] listOfUrls) 
+		{
+			//final array list that will contain all urls and words
+			 String[] allUrlsAndWordsArr;
+			 
+			 //Array list to help process the webpage
+			 List<String[]> allUrlsAndWordsList = new ArrayList<>();	
+			
+			//Default string[] to return in case DB connection fails
+			 String[] defaultReturn = {"Connection to DB could not be established"};
+			 
+			 System.out.println("--------- MySql JDBC Connetion Testing ------------");
+				
+				try
+				{
+					Class.forName("com.mysql.jdbc.Driver");
+				} 
+				
+				catch (ClassNotFoundException e) 
+				{
+					System.out.println("Where is your MySQL JDBC Driver?");
+					return defaultReturn;
+				}
+				
+				System.out.println("MySQL JDBC Driver Registered!");
+				Connection connection = null;
+				
+				try
+				{
+					connection = DriverManager
+							.getConnection("jdbc:mysql://localhost/javaDatabase","jwhitley", "password");
+				}
+				catch (SQLException e)
+				{
+					System.out.println("Connection Failed!! Check output console");
+					e.printStackTrace();
+					return defaultReturn;
+				}
+				
+				if (connection != null) 
+				{
+					System.out.println("Connection Succesful!");
+				} 
+				else 
+				{
+					System.out.println("Failed to make connection!");
+				}
+				
+		
+	//cycle through all of the urls passed into the method
+	 for(int i = 0; i < listOfUrls.length; i++)
+	 {
+		 	//Set url in super class to current url
+			super.setUrl(listOfUrls[i]);
+					 
+			//Read page for current url
+			super.readWebPage();
+					 
+			//Create a string array and parse the current url web page by spaces
+			Document doc = Jsoup.parse(super.getPage());
+					 
+					 
+			//Create a check to make sure that the url's are valid
+					 
+			//For loop to add each item from the currentUrlDataArray into the currentURLDataList
+			for(int j = 0; j < currentUrlDataArray.length; j++)
+				{
+					 allUrlsAndWordsList.add
+				}
+					 	
+					 	
+					 		
+					
+	 }
+				 
+				 
+			return allUrlsAndWordsArr;
+		}
+
     public static String[][] findKeywords(String[] urls){
 	    return null; 
     }
