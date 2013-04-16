010d2afd7775a5c5a43dcc7e4a739480557b7652
duncan <duncan@localhost.localdomain>
2013-04-16 12:13:32 -0600

    updated crawler

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -13,28 +13,34 @@ public class Crawler {
 	/**
 	 * sets the link scraper to the net real page 
 	 */
-	public static void runLinkScraper(){
-		LinkScraper links = new LinkScraper();
+	public static void runLinkScraper(KeywordDB DB){
 		//set link to last array
+		LinkScraper links = new LinkScraper(DB.getLastLink(0));
+		int counter = 1;
 		while(!links.scrape()){
-			//set links to last array -1 	
+			//set links to last array -1
+			links.setUrl(DB.getLastLink(counter)); 	
 		}
 	}
 	/**
 	 * sets the word scraper to the next real page 
 	 */
-	public static void runwordScraper(){
-		WordScraper words = new WordScraper(null);
+	public static void runwordScraper(KeywordDB DB){
+		WordScraper words = new WordScraper(DB.getFirstLink());
 		//set link = to first link with no date
 		while(!words.Scrape()){
+			DB.getFirstLink();
 			//set link to next array without a date
 		}
 	}
 	
 	public static void main(String... args){
+		KeywordDB DB = new KeywordDB();
+		//TODO: add error protection
+		DB.MakeDBConnection();
 		for (int i = 0; i < 10; i++) {
-			runLinkScraper();
-			runwordScraper();
+			runLinkScraper(DB);
+			runwordScraper(DB);
 		}
 	}
 	
